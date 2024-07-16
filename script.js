518d4302266f837cf2d242e1346ffb007edf19bb
Jefferson A. Arenas Z. <faceluker@outlook.es>
2024-07-15 20:52:48 -0500

    Update

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById('changeDescriptionBtn');
+const descriptionElement = document.getElementById('description');
+
+let isDescriptionChanged = true; // Variable de estado
+
+// Cambia la descripción y el texto del botón al hacer clic
+button.addEventListener('click', function() {
+    if (isDescriptionChanged) {
+        // Si ya se ha cambiado, vuelve a la descripción original
+        descriptionElement.textContent = "My skills range from programming in various languages and database management to the implementation of technological solutions that optimize business processes. I am able to work in all stages of the software lifecycle, from requirements analysis and planning to testing and deployment. My hands-on experience in real projects allows me to tackle complex problems with innovative and efficient solutions.";
+        button.textContent = "Volver";
+    } else {
+        // Si no se ha cambiado, establece la nueva descripción
+        descriptionElement.textContent = "As a software development technologist, I have a solid background in the design, implementation and maintenance of computer applications and systems.";
+        button.textContent = "Conocer Más";
+    }
+    
+    // Alterna el estado
+    isDescriptionChanged = !isDescriptionChanged;
+});
+
+// Mueve el botón al pasar el mouse
+button.addEventListener('mouseover', function() {
+    button.classList.add('move');
+});
+
+// Vuelve a la posición original al salir el mouse
+button.addEventListener('mouseout', function() {
+    button.classList.remove('move');
+});
